3bf8d24f65d9c984a57c9ccde9bc79f079067bb3
Johan Karlsson <DonKarlssonSan@gmail.com>
2022-12-03 10:23:41 +0100

    🩹 Fix template

--- template/b/b.spec.js
+++ template/b/b.spec.js
@@ -1,7 +1,7 @@
 import { assert } from "chai";
 import { b } from "./b.js";
 
-describe("0x-a ?", () => {
+describe("0x-b ?", () => {
   it("should return 1", () => {
     const input = `A Y`;
     const result = b(input);
