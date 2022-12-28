56c23e9b5085d6192b7cede140f3e20c5144799a
Johan Karlsson <DonKarlssonSan@gmail.com>
2022-12-28 12:23:43 +0100

    ✅ Add solution for day 10, part 2

--- 10/b/b.js
+++ 10/b/b.js
@@ -1,5 +1,44 @@
-export function b(inputString) {
-  const input = inputString.split("\n");
-
-  return 1;
+export function getPixels(inputString) {
+  const instructions = inputString.split("\n");
+  let x = 1;
+  let cycle = 1;
+  let pixels = [];
+  let nextOp = 1;
+  let instructionsIndex = 0;
+  while(true) {
+    const pos = (cycle - 1) % 40;
+    if(x === pos || x - 1 === pos || x + 1 === pos) {
+      pixels.push("#");
+    } else {
+      pixels.push(".")
+    }
+    if(nextOp === 0) {
+      const instruction = instructions[instructionsIndex];
+      const [command, value] = instruction.split(" ");
+      if(command === "addx") {
+        x += parseInt(value);
+        nextOp = 2;
+      } else if(command === "noop") {
+        nextOp = 1;
+      }
+      instructionsIndex++;
+    }
+    nextOp--;
+    cycle += 1;
+    if(instructionsIndex === instructions.length - 1) {
+      break;
+    }
+  }
+  let output = '';
+  for(let i = 0; i < pixels.length; i++) {
+    output += pixels[i];
+    if((i+1) % 40 === 0) {
+      output += "\n";
+    }
+  }
+  if(output[output.length-1] === "\n") {
+    output = output.substring(0, output.length-1);
+  }
+  //console.log(output);
+  return output;
 }
--- 10/run.js
+++ 10/run.js
@@ -1,10 +1,10 @@
 
 import { input } from "./input.js";
 import { getTotalSignalStrength } from "./a/a.js";
-import { b } from "./b/b.js";
+import { getPixels } from "./b/b.js";
 
 const resultA = getTotalSignalStrength(input);
 console.log(resultA);
 
-const resultB = b(input);
+const resultB = getPixels(input);
 console.log(resultB);
